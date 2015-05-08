3e582b1e0b81a833fa0c89ffb823f864011a697a
mike.moghadas <mmoghadas@rypple.com>
2015-05-08 11:36:07 -0700

    added bulk data

--- sofa.rb
+++ sofa.rb
@@ -40,6 +40,24 @@ class Sofa
     end
   end
 
+  def random_data(db, name='http_', count=2)
+    available_status = %w(healthy unhealthy)
+
+    count.times do |c|
+      service_name = name + c.to_s
+      status = {'status'=>available_status.sample}
+
+      begin
+        record = sit(:get, "/#{db}/#{service_name}")
+        rev = JSON.parse(record)['_rev']
+        sit(:put, "/#{db}/#{service_name}", status.merge('_rev'=>rev).to_json)
+      rescue
+        sit(:put, "/#{db}/#{service_name}", status.to_json)
+      end
+
+    end
+  end
+
   def handle_error(req, res)
     e = RuntimeError.new("#{res.code}:#{res.message}\nMETHOD: #{req.method}\nURI: #{uri}#{req.path}\n#{res.body}")
     raise e
